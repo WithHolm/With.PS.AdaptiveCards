f06428f7ecf14c3db9a41995847f8ff592127b18
Philip Meholm <philip.meholm@gmail.com>
2018-08-29 23:35:55 +0200

    new build

---
+++ With.PS.AdaptiveCards.CSharp/With.PS.AdaptiveCards.CSharp/cmdlet/NewAdaptiveAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using System.Management.Automation;
+
+namespace With.AdaptiveCards.CSharp
+{
+    [Cmdlet(VerbsCommon.New, "AdaptiveAction")]
+    public class NewAdaptiveAction : Cmdlet
+    {
+        [Parameter]
+        public string Speak { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+
+            // AdaptiveAction _Action = new AdaptiveAction();
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
---
+++ With.PS.AdaptiveCards.CSharp/With.PS.AdaptiveCards.CSharp/cmdlet/NewAdaptiveCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using System.Management.Automation;
+
+namespace With.AdaptiveCards.CSharp
+{
+    [Cmdlet(VerbsCommon.New, "AdaptiveCard")]
+    public class NewAdaptivecard:Cmdlet
+    {
+
+        [Parameter]
+        public string Speak { get; set; }
+
+        [Parameter]
+        public List<AdaptiveAction> Action { get; set; }
+
+        [Parameter]
+        public List<AdaptiveElement> Body { get; set; }
+
+        private AdaptiveCard _card { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            _card = new AdaptiveCard();
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            _card.ToJson();
+            base.EndProcessing();
+        }
+    }
+}
